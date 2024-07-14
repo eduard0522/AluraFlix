c3373d483d08796665334feb07935225f0c31006
eduard villamil <villamilandrey02@gmail.com>
2024-07-14 11:45:28 -0500

    Se cambio ruta vase de axios por la de producción

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9876/",
+  baseURL: "https://alura-flix-back.vercel.app/",
   withCredentials : true
 });
 
--- src/api/videos.js
+++ src/api/videos.js
@@ -3,7 +3,7 @@ import axios from "./axios.js";
 export const getVideos =  async () => {
 
   try {
-    const videos = await axios.get("/");
+    const videos = await axios.get("/api");
     if(!videos)   throw new Error("An unexpected error ocurred.");
     return videos.data
   } catch (error) {
@@ -14,7 +14,7 @@ export const getVideos =  async () => {
 export const getVideo =  async (id) => {
 
   try {
-    const video = await axios.get(`/${id}`);
+    const video = await axios.get(`/api/${id}`);
     if(!video)   throw new Error("An unexpected error ocurred.");
     return video.data
   } catch (error) {
@@ -24,7 +24,7 @@ export const getVideo =  async (id) => {
 
 export const createVideo = async (data) => {
   try {
-    const video = await axios.post("/",data);
+    const video = await axios.post("/api",data);
     console.log(video)
     return video.data
   } catch (error) {
@@ -34,7 +34,7 @@ export const createVideo = async (data) => {
 
 export const updateVideoRequest =  async (id, data) => {
   try {
-    const video  = await axios.put(`/${id}`, data);
+    const video  = await axios.put(`/api/${id}`, data);
     if( video.status === 404){
       return video
     }
@@ -47,7 +47,7 @@ export const updateVideoRequest =  async (id, data) => {
 
 export const deleteVideo = async (id) => {
   try {
-    const deleteVideo = await axios.delete(`/${id}`);
+    const deleteVideo = await axios.delete(`/api/${id}`);
     return deleteVideo.data
   } catch (error) {
     return error
